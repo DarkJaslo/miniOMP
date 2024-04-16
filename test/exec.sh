86325cea3f7932dbd47aa47777aa3c5c5f78eac7
Jon <jon.campillo@gmail.com>
2024-04-16 22:26:45 +0200

    add argument check

--- test/exec.sh
+++ test/exec.sh
@@ -1,1 +1,6 @@
+if [ $# -ne 1 ]; then
+    echo "Usage: exec <path-to-executable>"
+    exit
+fi
+
 LD_LIBRARY_PATH=../lib $1
--- test/time.sh
+++ test/time.sh
@@ -1,1 +1,6 @@
+if [ $# -ne 1 ]; then
+    echo "Usage: time <path-to-executable>"
+    exit
+fi
+
 time ./exec.sh $1
